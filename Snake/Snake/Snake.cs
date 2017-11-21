d201639ec3f54b2175ec2298c76525fa1cac7658
kibershot <praid145@gmail.com>
2017-11-21 10:42:22 +0500

    змейка ходит

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,8 +8,11 @@ namespace Snake
 {
     class Snake : Figure
     {
-        public Snake ( Point tail, int lenght, Direction direction)
+        Direction direction;
+
+        public Snake ( Point tail, int lenght, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
             for (int i = 0; i < lenght; i++)
             {
@@ -17,6 +20,24 @@ namespace Snake
                 p.Move(i, direction);
                 pList.Add(p);
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First(); //Возвращает первый элемент списка
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear(); //затираем последнюю точку хвоста и рисуем новую голову
+            head.Draw();
+        }
+        public Point GetNextPoint() // дай мне следующую точку
+        {
+            Point head = pList.Last(); // текущая позиция головы
+            Point nextPoint = new Point(head); // новая точка копия предыдущего положения головы
+            nextPoint.Move(1, direction); // сдвигаем по направлению дирекшен
+            return nextPoint;
         }
     }
 }
